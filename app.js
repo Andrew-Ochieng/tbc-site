b94b35364322c67d4f0727ed4b3ff3f29d4cd4fb
AndrewMartial <andrewomosh44@gmail.com>
2022-07-28 23:06:09 +0300

    change file structure && js toggle menu function

---
+++ app.js
@@ -0,0 +1,26 @@
+// toggle menu-bar
+const btn = document.querySelector("button.menu-btn");
+const menu = document.querySelector(".menu");
+
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
