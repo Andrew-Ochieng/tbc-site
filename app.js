d0f2cfa8d69d762e0c2be93fd56e3f8e3be4bfbe
AndrewMartial <andrewomosh44@gmail.com>
2022-07-28 23:52:06 +0300

    add copyright time-update

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // toggle menu-bar
 const btn = document.querySelector("button.menu-btn");
 const menu = document.querySelector(".menu");
+const year = document.getElementById('copyright')
 
-
+// toggle menubar
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
+
 });
 
 
+// display current year
+const currentYear = new Date().getFullYear()
 
-
-
+year.innerHTML = currentYear
 
 
 
